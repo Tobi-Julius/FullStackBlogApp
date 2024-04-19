c2c56e79ab7b4eaa206c1ba402c8abb70df7c77d
tobi-julius <tobi.julius@centricwave.co>
2024-04-19 22:55:37 +0100

    full stack blog project

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+// const express = require("express");
+
+// const app = express();
+
+// const PORT = process.env.PORT || 9000;
+
+// app.listen(PORT, () => {
+//   console.log(`server running on PORT ${PORT}`);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>This is meant to be used for react native</h2>");
+// });
+
+const mysql = require("mysql");
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "ABC123@ ",
+  //   database: "",
+});
+
+con.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("was successful");
+  }
+});
