8be743ce82b21074b617046164c66d05d2df4fa1
tobi-julius <tobi.julius@centricwave.co>
2024-06-15 18:57:13 +0100

    progress

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`server running on PORT ${PORT}`); 
+  console.log(`server running on PORT ${PORT}`);
 });
 
 app.get("/", (req, res) => {
